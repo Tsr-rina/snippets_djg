3661253bcaa55ed5f8fe23e88d339e36e09b9891
rinas <topi2frie3elec@gmail.com>
2022-12-05 16:21:09 +0900

    first init

---
+++ snippets/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.http import HttpRequest
+from snippets.views import top
+# Create your tests here.
+
+class TopPageViewTest(TestCase):
+    def test_top_returns_200(self):
+        request = HttpRequest()
+        response = top(request)
+        self.assertEqual(response.status_code, 200)
+    def test_top_returns_expected_content(self):
+        request = HttpRequest()
+        response = top(request)
+        self.assertEqual(response.content, b"Hello World")
