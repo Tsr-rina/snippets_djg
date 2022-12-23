cc75d0739333bc0c74dccc162af5454454308e67
rinas <topi2frie3elec@gmail.com>
2022-12-23 18:53:53 +0900

    add extends

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
+            # 以下を設定しておけばテンプレ内でrequestでHttpRequestオブジェクトにアクセスできる
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
